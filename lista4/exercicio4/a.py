4f7770e2a73a14bfd65fa5a578e76275c9c6bb2d
Hugo Souza <hugo.souza0610@gmail.com>
2023-04-12 18:38:27 -0400

    Adicionado a pesquisa de parentesco na arvore

--- lista4/exercicio4/a.py
+++ lista4/exercicio4/a.py
@@ -7,19 +7,55 @@ class Membro:
     def adicionarFilho(self, nome):
         self.filhos.append(Membro(nome, self))
 
-    def encontrarParentesco(self, pessoa):
-        fila_de_pesquisa = [self.pai] + self.filhos
+    def encontrarParentesco(self, pessoa, raiz=True, membros_pesquisados=[]):
+        encontrado = False
+        linha_de_parentesco = ""
 
-        while (len(fila_de_pesquisa) > 0):
-            # procurar  
-            proximo_candidato = fila_de_pesquisa.pop(0)
+        membros_pesquisados.append(self.nome)
 
+        if self.pai != None and not (self.pai.nome in membros_pesquisados):
+            if self.pai.nome == pessoa:
+                linha_de_parentesco = "filho do "
+                encontrado = True
+            else:
+                parentesco_do_pai = self.pai.encontrarParentesco(pessoa, False, membros_pesquisados)
+                if parentesco_do_pai != "nada do ":
+                    linha_de_parentesco = "filho do " + parentesco_do_pai
+                    encontrado = True
 
-            # expandir
+        if not encontrado:
+            for filho in self.filhos:
+                if filho.nome == pessoa and not (filho.nome in membros_pesquisados):
+                    linha_de_parentesco = "pai do "
+                    encontrado = True
+                else:
+                    parentesco_do_filho = filho.encontrarParentesco(pessoa, False, membros_pesquisados)
+                    if parentesco_do_filho != "nada do ":
+                        linha_de_parentesco = "pai do " + parentesco_do_filho
 
+        if linha_de_parentesco == "":
+            linha_de_parentesco = "nada do "
 
+        if raiz:
 
-# Use the insert method to add Membros
+            while True:
+                alterado = False
+
+                if linha_de_parentesco.find("filho do filho do ") != -1:
+                    alterado = True
+                    linha_de_parentesco = linha_de_parentesco.replace("filho do filho do ", "neto do ")
+                
+                if linha_de_parentesco.find("filho do pai do ") != -1:
+                    alterado = True
+                    linha_de_parentesco = linha_de_parentesco.replace("filho do pai do ", "tio do ")
+            
+                if not alterado:
+                    break
+
+            print(f"{self.nome} é {linha_de_parentesco}{pessoa}")
+        else:
+            return linha_de_parentesco
+
 vo = Membro("João", None)
 
 vo.adicionarFilho("Maria")
@@ -30,3 +66,5 @@ vo.filhos[0].adicionarFilho("Juliana")
 
 vo.filhos[1].adicionarFilho("Julio")
 vo.filhos[1].adicionarFilho("Felipe")
+
+vo.filhos[1].filhos[1].encontrarParentesco("Maria")

c9f1be46b5c5bba3e66fbf93c7ab776a7f9eddbd
fatorius <hugo.souza0610@gmail.com>
2023-04-20 00:18:19 -0400

    Adicionado novas funcionalidades na prova

--- prova1/prova.py
+++ prova1/prova.py
@@ -16,10 +16,10 @@ def instalar_pacote(pacote):
 try:
     import pandas as pd
 except ModuleNotFoundError:
-    print("A biblioteca pandas não está instalada no seu computador. "
-          + "Irei tentar instalar pandas automaticamente para você "
-          + "Essa biblioteca é necessária para tratar de arquivos .xlsx "
-          + "Se não der certo, por favor, instale manualmente executando o comando: "
+    print("A biblioteca pandas não está instalada no seu computador. \n"
+          + "Irei tentar instalar pandas automaticamente para você \n"
+          + "Essa biblioteca é necessária para tratar de arquivos .xlsx \n"
+          + "Se não der certo, por favor, instale manualmente executando o comando: \n"
           + "python3 -m pip install pandas")
     instalar_pacote("pandas")
 
@@ -28,20 +28,20 @@ try:
 
     init(autoreset=True)
 except ModuleNotFoundError:
-    print("A biblioteca colorama não está instalada no seu computador. "
-          + "Irei tentar instalar colorama automaticamente para você "
-          + "Essa biblioteca é necessária para usar exibir textos coloridos no seu terminal "
-          + "Se não der certo, por favor, instale manualmente executando o comando: "
+    print("A biblioteca colorama não está instalada no seu computador. \n"
+          + "Irei tentar instalar colorama automaticamente para você \n"
+          + "Essa biblioteca é necessária para usar exibir textos coloridos no seu terminal \n"
+          + "Se não der certo, por favor, instale manualmente executando o comando: \n"
           + "python3 -m pip install colorama")
     instalar_pacote("colorama")
 
 try:
     from termcolor import colored
 except ModuleNotFoundError:
-    print("A biblioteca termcolor não está instalada no seu computador. "
-          + "Irei tentar instalar colorama automaticamente para você "
-          + "Essa biblioteca é necessária para usar exibir textos coloridos no seu terminal "
-          + "Se não der certo, por favor, instale manualmente executando o comando: "
+    print("A biblioteca termcolor não está instalada no seu computador. \n"
+          + "Irei tentar instalar colorama automaticamente para você \n"
+          + "Essa biblioteca é necessária para usar exibir textos coloridos no seu terminal \n"
+          + "Se não der certo, por favor, instale manualmente executando o comando: \n"
           + "python3 -m pip install termcolor")
     instalar_pacote("termcolor")
 
@@ -51,6 +51,8 @@ def exibir_mensagens_de_boas_vindas():
 
 
 def menu_de_opcoes():
+    print()
+
     print(colored("Digite a opção desejada: ", "white", "on_yellow"))
     print(colored("1 - Informar salario", "yellow"))
     print(colored("2 - Alterar salario", "yellow"))
@@ -61,14 +63,18 @@ def menu_de_opcoes():
     print(colored("7 - Remover despesa", "yellow"))
     print(colored("8 - Listar despesa", "yellow"))
     print(colored("9 - Mostar resultado", "yellow"))
+    print(colored("0 - Sair", "yellow"))
+
+    print()
 
     try:
-        opcao_selecionada = int(input(">> "))
+        opcao_selecionada = int(input(colored("joao-serasa: ~$ ", "light_green")))
     except:
         print(colored("Erro: Opção informada inválida", "red"))
         return menu_de_opcoes()
 
-    if 0 < opcao_selecionada < 10:
+
+    if 0 <= opcao_selecionada < 10:
         return opcao_selecionada
     else:
         print(colored("Erro: Opção informada inválida", "red"))
@@ -91,20 +97,20 @@ def informar_salario():
         mes_atual = 1
         ano_atual += 1
 
-    print(colored(f"Informe o seu salário para o mês {mes_atual} de {ano_atual}", "white", "on_green"))
+    print(colored(f"Informe o seu salário para o mês {mes_atual} de {ano_atual}", "white", "on_blue"))
 
     salario_atual = 0
 
     try:
-        salario_atual = int(input())
+        salario_atual = int(input(colored("joao-serasa: ~$ ", "light_green")))
     except:
         print(colored("Salário digitado inválido, tente novamente: ", "red"))
         informar_salario()
 
     tabela.loc[tamanho] = {"Mes": mes_atual,"Ano": ano_atual,"Valor": salario_atual,"Tipo": "Salario",
                             "Saldo": saldo_anterior + salario_atual}
-
-    tabela.to_csv("tabela.csv")
+    
+    print(colored(f"Salário adicionado com sucesso", "white", "on_blue"))
 
 def excluir_salario():
     pass
@@ -119,8 +125,32 @@ def listar_salario():
 
 
 def informar_despesa():
-    pass
+    tamanho = tabela.shape[0]
+    saldo_anterior = tabela.at[tamanho-1, "Saldo"]
+    mes_anterior = tabela.at[tamanho-1, "Mes"]
+    ano_anterior = tabela.at[tamanho-1, "Ano"]
 
+    mes_atual = mes_anterior + 1
+    ano_atual = ano_anterior
+
+    if mes_atual == 13:
+        mes_atual = 1
+        ano_atual += 1
+
+    print(colored(f"Informe as suas despesas para o mês {mes_atual} de {ano_atual}", "white", "on_red"))
+
+    despesa_atual = 0
+
+    try:
+        despesa_atual = int(input(colored("joao-serasa: ~$ ", "light_green")))
+    except:
+        print(colored("Despesa digitada inválida, tente novamente: ", "red"))
+        informar_salario()
+
+    tabela.loc[tamanho] = {"Mes": mes_atual,"Ano": ano_atual,"Valor": despesa_atual,"Tipo": "Despesa",
+                            "Saldo": saldo_anterior - despesa_atual}
+    
+    print(colored(f"Despesas registradas com sucesso", "white", "on_red"))
 
 def alterar_despesa():
     pass
@@ -135,7 +165,9 @@ def listar_despesas():
 
 
 def mostrar_resultados():
-    pass
+    print(tabela)
+    print(colored("Aperte enter para continuar", "white", "on_cyan"))
+    input()
 
 
 def carregar_tabela():
@@ -144,21 +176,26 @@ def carregar_tabela():
         tabela = pd.read_csv("tabela.csv")
     except FileNotFoundError:
         with open("tabela.csv", 'w') as nova_tabela:
-            nova_tabela.write("Mes, Ano, Valor, Tipo, Saldo\n")
-            nova_tabela.write('0, 0, 0, "Inicio", 0')
+            nova_tabela.write("id,Mes,Ano,Valor,Tipo,Saldo\n")
+            nova_tabela.write('0,0,2023,0,Inicio,0')
             nova_tabela.close()
 
-        tabela = pd.read_csv("tabela.csv")
+        tabela = pd.read_csv("tabela.csv", index_col="id")
 
+def encerrar_programa():
+    print(colored("Encerrando programa...", "white", "on_red"))
+    exit()
 
 def iniciar_programa():
     carregar_tabela()
 
     exibir_mensagens_de_boas_vindas()
 
-    ["", informar_salario, alterar_salario, excluir_salario,
-     listar_salario, informar_despesa, alterar_despesa, remover_despesa,
-     listar_despesas, mostrar_resultados][menu_de_opcoes()]()
-
+    while True:
+        [encerrar_programa, informar_salario, alterar_salario, excluir_salario,
+         listar_salario, informar_despesa, alterar_despesa, remover_despesa,
+         listar_despesas, mostrar_resultados][menu_de_opcoes()]()
+    
+        tabela.to_csv("tabela.csv", index_label=False)
 
 iniciar_programa()
